08e6c8ae32d3f0e0bcc42997885288f7b3f15459
hilalakgul <hilalakgul9393@outlook.com>
2023-05-28 17:50:14 +0300

    edit1

---
+++ EcommerceProjectNLayer/Utility/Dtos/CartListDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Business.Dtos
+{
+	public class CartListDto
+	{
+        public int Unit { get; set; }
+        
+    }
+}
+
---
+++ EcommerceProjectNLayer/Utility/Dtos/ProductListDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Entities;
+
+namespace Business.Dtos
+{
+	public class ProductListDto
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+    }
+}
+
